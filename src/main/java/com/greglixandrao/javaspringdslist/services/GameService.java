ab0da0162a4161557d733826a4aec2757f21151c
Greg Lixandrão <contato@greglixandrao.com>
2023-07-12 19:10:33 -0300

    Add findById in GameService with Transacional Annotation

--- src/main/java/com/greglixandrao/javaspringdslist/services/GameService.java
+++ src/main/java/com/greglixandrao/javaspringdslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.greglixandrao.javaspringdslist.services;
 
+import com.greglixandrao.javaspringdslist.dto.GameDTO;
 import com.greglixandrao.javaspringdslist.dto.GameMinDTO;
 import com.greglixandrao.javaspringdslist.entities.Game;
 import com.greglixandrao.javaspringdslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,8 +14,15 @@ import java.util.List;
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> resultado = gameRepository.findAll();
         return resultado.stream().map(x -> new GameMinDTO(x)).toList();
     }
+
 }
