9da10ce834d1cc97c4833a632b76c9e007579e26
Greg Lixandrão <contato@greglixandrao.com>
2023-07-12 16:22:01 -0300

    Initial Commit

---
+++ src/main/java/com/greglixandrao/javaspringdslist/JavaSpringDslistApplication.java
@@ -0,0 +1,13 @@
+package com.greglixandrao.javaspringdslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDslistApplication.class, args);
+	}
+
+}
