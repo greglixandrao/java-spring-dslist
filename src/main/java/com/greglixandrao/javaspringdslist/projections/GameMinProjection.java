260e0b890121bcd7729b0a842a16bccc6d531e7a
Greg Lixandrão <contato@greglixandrao.com>
2023-07-14 10:46:33 -0300

    Refactor SQL Query in GameRepository to work with PostgreSQL, and refactor Projection and DTO

--- src/main/java/com/greglixandrao/javaspringdslist/projections/GameMinProjection.java
+++ src/main/java/com/greglixandrao/javaspringdslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.greglixandrao.javaspringdslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
